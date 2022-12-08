7ddbd23701abb8a06dcd5ac5e704f2aa6ea228de
Kauan Torrisi <torrisikauan@gmail.com>
2022-12-08 09:40:53 -0300

    first commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Counter App'),
+      ),
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
